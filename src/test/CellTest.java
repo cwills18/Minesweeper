251608b09b4ae5c7804e5361e2bf6729e8d5e703
charise <charise.r.wills@gmail.com>
2023-05-05 16:51:52 +1000

    converted to maven

---
+++ src/test/CellTest.java
@@ -0,0 +1,16 @@
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CellTest {
+
+    @Test
+    void testing_test_Tests(){
+        int num1 = 5;
+        int num2 = 10;
+        assertEquals(15, num1 + num2);
+    }
+
+}
---
+++ src/test/OrdinaryCellTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrdinaryCellTest {
+	
+    static OrdinaryCell cell;
+    
+//    @BeforeEach
+//    void setUpCell() {
+//        cell = new main.java.OrdinaryCell(1);
+//    }
+
+//    @Test
+//    void setSurroundingMines_PositiveInteger_SetsFieldCorrectly() {
+//        cell.setSurroundingMines(5);
+//        int result = cell.surroundingMines;
+//        assertEquals(5, result);
+//    }
+
+   @Test
+    void testing_test_Tests(){
+       int num1 = 5;
+       int num2 = 10;
+       assertEquals(15, num1 + num2);
+    }
+}
